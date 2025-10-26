3220a8214c5cd0d4929bb13e81060ba0566721ac
Angela S. Milford <angela.milford123@gmail.com>
2025-10-25 15:46:57 -1000

    Client-side Data Validation

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,14 @@
 // Own Today button
 function goToPage() {
-    window.location.href = "#";
+  window.location.href = "#";
 }
 
 function togglePassword() {
-    const pwField = document.getElementById('account_password');
-    pwField.type = pwField.type === 'password' ? 'text' : 'password';
+  const passwordInput = document.getElementById("account_password");
+  passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+}
+
+function togglePassword() {
+  const passwordInput = document.getElementById("account_password");
+  passwordInput.type = passwordInput.type === "password" ? "text" : "password";
 }
